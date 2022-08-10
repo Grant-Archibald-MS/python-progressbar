92cf22d4642e493aa3e02d4337b9219f374bc228
Grant Archibald <31553604+Grant-Archibald-MS@users.noreply.github.com>
2022-08-09 18:53:27 -0700

    Adding hotkeys and docs

--- README.md
+++ README.md
@@ -1,6 +1,24 @@
 # Python Progress Timer
 
-Minimal sample application that uses Python tkinker module to implement a top of screen overlay
+“Time – the one asset none of us are ever going to get more of.”
+– Gary Vaynerchuk
+
+We have all been in meeting and presentations and found it difficult to manage our time and get keep on time. The Python Progress bar is a very simple sample application that provides a visual guide to your audience to help show progress.
+
+The sample application is implemented to be placed over top any window on the main display. As a result it can be included on top of an exiting presentation or live screen sharing session to track progress.
+
+By being outside a presentation or slide you can switch between slides or across applications and still provide the visual vue on progress.
+
+## Hotkeys
+
+The application has the following hot keys defined
+<ctrl>+<alt>+0 - Clear Timer
+<ctrl>+<alt>+1 - <ctrl>+<alt>+9 Set Time from times.txt
+<ctrl>+<shift>+1 - <ctrl>+<shift>+9 Set Time Extra time 1 - 5 mins
+
+## Technical Details
+
+Minimal sample application that uses Python [tkinker](https://docs.python.org/3/library/tkinter.html) module to implement a top of screen overlay.
 
 ## Getting Started
 
--- timer.py
+++ timer.py
@@ -5,6 +5,7 @@
 # 2. python timer.py
 from tkinter import ttk
 from tkinter import *
+from pynput import keyboard
 import time
 
 from threading import Thread
@@ -16,7 +17,7 @@ total = IntVar()
 run = BooleanVar()
 run.set(False)
 extra = BooleanVar()
-run.set(False)
+extra.set(False)
 timeLimit.set(0)
 total.set(80)
 label = StringVar()
@@ -46,6 +47,40 @@ p = ttk.Progressbar(root, orient="horizontal", length=200, mode="determinate",
 p['value'] = 0
 p.pack()
 
+times = []
+
+with open('times.txt') as f:
+    times = f.read().splitlines()
+
+# Use python thread to allow the user to 
+class HotKeysThread(Thread):
+    def __init__(self):
+        Thread.__init__(self)
+        self.start()
+
+    def run(self):
+        # Setup Hot Keys
+        with keyboard.GlobalHotKeys({
+                '<ctrl>+<alt>+0': setTime0,
+                '<ctrl>+<alt>+1': setTime1,
+                '<ctrl>+<alt>+2': setTime2,
+                '<ctrl>+<alt>+3': setTime3,
+                '<ctrl>+<alt>+4': setTime4,
+                '<ctrl>+<alt>+5': setTime5,
+                '<ctrl>+<alt>+6': setTime6,
+                '<ctrl>+<alt>+7': setTime7,
+                '<ctrl>+<alt>+8': setTime8,
+                '<ctrl>+<alt>+9': setTime9,
+                '<ctrl>+<shift>+1': setExtraTime1,
+                '<ctrl>+<shift>+2': setExtraTime2,
+                '<ctrl>+<shift>+3': setExtraTime3,
+                '<ctrl>+<shift>+4': setExtraTime4,
+                '<ctrl>+<shift>+5': setExtraTime5}) as h:
+            h.join()
+
+    def setTime(val):
+        print(val)
+
 # Use python thread to allow the user to 
 class MyThread(Thread):
     def __init__(self):
@@ -54,70 +89,92 @@ class MyThread(Thread):
 
     def run(self):
         while True:
-            user_input = input("What do I do ? ")
+            user_input = input("What do I do (e or minutes) ? ")
             print(user_input)
 
             # User wishes to exit
             if user_input == 'e':
                 sys.exit()
 
+            processKey(user_input)
+            
 
-            timeLimit.set(0)
-            extra.set(False)
+def processKey(val):
+    timeLimit.set(0)
+    extra.set(False)
 
-            if user_input.startswith("e"):
-                extra.set(True)
-                user_input = user_input[1:]
+    if val.startswith("e"):
+        extra.set(True)
+        val = val[1:]
+
+    if val.isdigit():
+        # Set time progress bar in minutes
+        setTime(val)
+
+def clearTime():
+    timeLimit.set(0)
+    style.configure("red.Horizontal.TProgressbar", text='', foreground='black', background='green')
+    run.set(False) 
+
+def setTime0():
+    processKey('0')
+
+def setTime1():
+    processKey(times[0])
+
+def setTime2():
+    processKey(times[1])
+
+def setTime3():
+    processKey(times[2])
+
+def setTime4():
+    processKey(times[3])
+
+def setTime5():
+    processKey(times[4])
+
+def setTime6():
+    processKey(times[5])
+
+def setTime7():
+    processKey(times[6])
+
+def setTime8():
+    processKey(times[7])
+
+def setTime9():
+    processKey(times[8])
 
 
-            if user_input == '0':
-                # User wants to pause timer and reset
-                style.configure("red.Horizontal.TProgressbar", foreground='black', background='green')
-                run.set(False)
+def setExtraTime1():
+    processKey('e1')
 
-            # Set time progress bar in minutes (1-10)
-            if user_input == '1':
-                total.set(60)
-                p['value'] = 0
-                run.set(True)
-                label.set("1 min")
+def setExtraTime2():
+    processKey('e2')
 
-            if user_input == '2':
-                total.set(120)
-                p['value'] = 0
-                run.set(True)
-                label.set("2 mins")
+def setExtraTime3():
+    processKey('e3')
 
-            if user_input == '3':
-                total.set(180)
-                p['value'] = 0
-                run.set(True)
-                label.set("3 mins")
+def setExtraTime4():
+    processKey('e4')
 
-            if user_input == '4':
-                total.set(240)
-                p['value'] = 0
-                run.set(True)
-                label.set("4 mins")
+def setExtraTime5():
+    processKey('e5')
 
-            if user_input == '5':
-                total.set(300)
-                p['value'] = 0
-                run.set(True)
-                label.set("5 mins")
+def setTime(value):
+    if value == '0':
+        clearTime()
+        return
 
-            if user_input == '6':
-                total.set(360)
-                p['value'] = 0
-                run.set(True)
-                label.set("6 mins")
-
-            if user_input == '10':
-                total.set(600)
-                p['value'] = 0
-                run.set(True)
-                label.set("10 mins")
-
+    minutes = int(value)
+    total.set(minutes * 60)
+    p['value'] = 0
+    run.set(True)
+    if minutes == 1:
+        label.set("1 min")
+    else:
+        label.set(value + " mins")
 
 def start():
     # Make sure that the window is the top most over top of other windows
@@ -127,20 +184,31 @@ def start():
     # Check if running and less than 100% complete
     if p['value'] <= 100 and run.get():
         variable.set(round(timeLimit.get() / total.get() * 100,0))
-        foregroundColor = 'black'
-        backgroundColor = 'green'
-
-        if p['value'] > 60 :
-            backgroundColor = 'orange'
-
-        if p['value'] > 80 :
-            foregroundColor = 'white'
-            backgroundColor = 'red'
 
         extraLabel = ''
         if extra.get():
             extraLabel = 'EXTRA '
+            foregroundColor = 'black'
+            backgroundColor = 'red'
+        else:
+            foregroundColor = 'black'
+            backgroundColor = 'green'
 
+        if p['value'] > 60 :
+            if extra.get():
+                foregroundColor = 'white'
+                backgroundColor = 'brown'
+            else:
+                backgroundColor = 'orange'
+
+        if p['value'] > 80 :
+            if extra.get():
+                backgroundColor = 'purple'
+            else:
+                foregroundColor = 'white'
+                backgroundColor = 'red'
+
+        
         style.configure('red.Horizontal.TProgressbar', 
                     text='{0}{1} - {2:g} %'.format(extraLabel, label.get(), variable.get()), foreground=foregroundColor, background=backgroundColor)  # update label
         timeLimit.set(timeLimit.get()+1)
@@ -153,6 +221,18 @@ def start():
 
     # Check again in another second
     root.after(1000, start)
+
+
+print('Progress Bar')
+print('============')
+print()
+print("Press Hot Keys <ctrl>-<alt>-1 to  <ctrl>-<alt>-9 to set default times")
+print(times)
+print("Press Hot Keys <ctrl>-<shift>-1 to  <ctrl>-<shift>-5 to set extra times")
+print(["1","2","3","4","5"])
+print()
+
+keys = HotKeysThread()
 
 # Open the window
 my = MyThread()
